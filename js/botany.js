06e80c7f0a9d88a5f04656f27dec26434bf51731
Aislinn Mumford <mumforda@tcd.ie>
2019-11-19 17:00:29 +0000

    Trying to fix button problem

--- js/botany.js
+++ js/botany.js
@@ -1,5 +1,6 @@
-
+var span = document.getElementsByClassName("close")[0];
 // Get the modal
+
 var modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
@@ -7,15 +8,16 @@ var img = document.getElementById("myImg");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
+modal.style.display = "block";
+modalImg.src = this.src;
+captionText.innerHTML = this.alt;
 }
-
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+modal.style.display = "none";
+}
+function closeModal() {
+document.getElementById("myModal").style.display = "none";
 }
